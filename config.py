97df4682c231f12de4438ce4e75f30706f8b3aac
YangJ-PG <160707281+YangJ-PG@users.noreply.github.com>
2025-02-18 15:11:29 +0800

    Create config.py
    配置文件

---
+++ config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+loaction = "D:\同花顺"
+# 概念&概念下股票列表
+stockBlock_url = loaction + "\system\同花顺方案\stockBlock.ini"
+# 概念代码
+stockLink_url = loaction + "\system\同花顺方案\stockLink.ini"
+# 概念关联度
+conceptRelation_url = loaction + "\system\同花顺方案\ConceptRelation.json"
+
+
+# 数据库连接
+host = "127.0.0.1"
+user = "root"
+port = 3352
+password = "66668888"
+database = "stock"
+
+# 日志文件目录
+filename = "D:\\phpworkspaces\\project\\stock\log\\"
+logname = "D:\\phpworkspaces\\project\\stock\log\\mylog.log"
